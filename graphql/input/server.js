0da527ec197c46fa2bf95c18091adb75a8b62b40
Michael <mbg@outlook.com>
2017-10-20 09:46:01 +0100

    Update server.js

--- graphql/input/server.js
+++ graphql/input/server.js
@@ -3,13 +3,12 @@ import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 import bodyParser from 'body-parser'
 import schema from './schema'
 
-const GRAPHQL_PORT = 8000
-
+const PORT = 8000
 const server = express()
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
 server.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
-server.listen(GRAPHQL_PORT, () =>
+server.listen(PORT, () =>
   console.log(`GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`)
 )
