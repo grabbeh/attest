d3ee5e4b852060681b49f5e055bf71efcae4b773
Michael <mbg@outlook.com>
2017-11-12 21:22:42 +0000

    Update

--- graphql/input/server.js
+++ graphql/input/server.js
@@ -18,7 +18,21 @@ const corsOptions = {
 const server = express()
 
 // server.use(cors(corsOptions))
-server.use('*', cors(corsOptions))
+// server.use('*', cors(corsOptions))
+
+app.use('/graphql', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  )
+  if (req.method === 'OPTIONS') {
+    console.log('Method = OPTIONS')
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
 
 server.use(
   '/graphql',
