7dacfb79372e99b05f0b4f5525058a778ea1001f
Michael <mbg@outlook.com>
2017-11-12 19:24:33 +0000

    Update for cors

--- graphql/input/server.js
+++ graphql/input/server.js
@@ -3,20 +3,10 @@ import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 import bodyParser from 'body-parser'
 import schema from './schema'
 import cors from 'cors'
-// import config from '../config/db'
-// import database from './db'
-
-/* const db = mongoose.connect('mongodb://grabbeh:everton@207.154.247.21/test')
-console.log(db)
-const context = { db }
-context.db = db */
 
 const GRAPHQL_PORT = 8000
 
-const whitelist = [
-  // Allow domains here
-  'http://localhost:3000'
-]
+const whitelist = ['http://localhost:3000']
 const corsOptions = {
   origin (origin, callback) {
     const originIsWhitelisted = whitelist.indexOf(origin) !== -1
@@ -26,6 +16,7 @@ const corsOptions = {
 }
 
 const server = express()
+server.options('*', cors())
 server.use(cors(corsOptions))
 server.use('/graphql', bodyParser.json(), graphqlExpress({ schema: schema }))
 server.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
