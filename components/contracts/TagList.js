86befc8ace9bdf1b9cd160506d1427925489bb04
Michael <mbg@outlook.com>
2017-11-28 23:12:55 +0000

    Style

--- components/contracts/TagList.js
+++ components/contracts/TagList.js
@@ -1,16 +1,14 @@
 const TagList = ({ tags }) => (
-  <div className='fl'>
-    <ul className='list ma0 pa0'>
-      {tags.map(tag => (
-        <li
-          key={tag}
-          className='shadow-4 b f6 bg-dark-sur white fl pv1 ph2 mr2 mv1'
-        >
-          {tag}
-        </li>
-      ))}
-    </ul>
-  </div>
+  <ul className='mt1 list ma0 pa0'>
+    {tags.map(tag => (
+      <li
+        key={tag}
+        className='shadow-4 b f6 bg-dark-sur white fl pv1 ph2 mr2 mv1'
+      >
+        {tag}
+      </li>
+    ))}
+  </ul>
 )
 
 export default TagList
