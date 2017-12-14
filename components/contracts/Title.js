f905e01a5dacb052d3d1f90bffa048b3ddd6f7e2
Michael <mbg@outlook.com>
2017-12-14 16:42:48 +0000

    Update Title.js

--- components/contracts/Title.js
+++ components/contracts/Title.js
@@ -1,6 +1,6 @@
 const Title = ({ name }) => (
   <div className='w-50-ns w-100'>
-    <div className='bg-haus pa3 mr3 mb3 f3 b'>{name}</div>
+    <div className='bg-haus pa3 mr3-l mr0 mb3 f3 b'>{name}</div>
   </div>
 )
 
