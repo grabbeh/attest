1747a26e4bdf72962df070ac1cf3ac203bafd1a2
Michael <mbg@outlook.com>
2017-12-14 08:02:05 +0000

    Update to style

--- components/contracts/ContractHolder.js
+++ components/contracts/ContractHolder.js
@@ -3,10 +3,12 @@ import cn from 'classnames'
 const ContractHolder = ({ children, index }) => (
   <li
     className={cn(
-      (index + 1) % 4 === 0 && 'mr0-ns',
-      (index + 1) % 4 !== 0 && 'mr3-ns',
+      (index + 1) % 4 === 0 && 'mr0-l',
+      (index + 1) % 4 !== 0 && 'mr3-l',
       (index + 1) % 4 === 0 && 'mr0',
       (index + 1) % 4 !== 0 && 'mr0',
+      (index + 1) % 2 === 0 && 'mr0-ns',
+      (index + 1) % 2 !== 0 && 'mr3-ns',
       'bg-haus',
       'f5',
       'pa3',
