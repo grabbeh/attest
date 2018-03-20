5f17eed1c6a55c7562c4a62ebbf8657c33e2fee0
Michael <mbg@outlook.com>
2018-03-20 23:09:18 +0000

    Search tweak

--- components/contracts/SearchInput.js
+++ components/contracts/SearchInput.js
@@ -4,15 +4,21 @@ import react from 'react'
 class SearchInput extends react.Component {
   render () {
     return (
-      <div className='w-50-l w-100 pv1 f4'>
+      <div className='w-50-l w-100 f4'>
         <span className='b fl mt2 mr2'>Search</span>
         <input
-          className='fl w-100 bb bw1 b--black-20 font pa1 mw5'
+          className='mt1 fl w-100 bb bw1 b--black-20 font pa1 mw5'
           value={this.props.searchTerm}
           onChange={this.props.handleSearchInput}
         />
         <span
-          className={cn(!this.props.searchTerm && 'dn', 'fl', 'pa1', 'b')}
+          className={cn(
+            !this.props.searchTerm && 'dn',
+            'mt1',
+            'fl',
+            'pa1',
+            'b'
+          )}
           onClick={this.props.clear}
         >
           <i className='fa fa-close' />
