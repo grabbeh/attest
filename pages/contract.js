cbfded542ff1b8adf136f8527532e4ed702f876b
Michael <mbg@outlook.com>
2018-04-15 01:43:07 +0100

    Removed contract.js

--- pages/contract.js
+++
@@ -1,6 +0,0 @@
-import ContractContainer from '../components/contracts/ContractContainer'
-import { AuthPage } from '../components/Page'
-
-const ContractsPage = AuthPage(props => <ContractContainer {...props} />)
-
-export default ContractsPage
