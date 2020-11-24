b0b5504f79eb2bad5ad4c776b2f6cb9c4319216d
Michael <mbg@outlook.com>
2020-11-24 12:21:45 +0000

    Auth update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,19 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import dotenv from 'dotenv'
+dotenv.config({ path: '../../.env' })
 
 const options = {
   // Configure one or more authentication providers
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+    Providers.Email({
+      host: process.env.EMAIL_SERVER_HOST,
+      port: process.env.EMAIL_SERVER_PORT,
+      auth: {
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD
+      },
+      from: process.env.EMAIL_FROM
     })
     // ...add more providers here
   ],
